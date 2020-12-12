1a9de62860b2e54582980baffc65ebd800e8f816
Dinesh Kanchi <k.d.v.kumar123@gmail.com>
2020-12-12 11:16:54 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -48,7 +48,10 @@ const App = () => {
 
       <div className="row" id="children-wrapper">
         {["selection1", "selection2", "selection3"].map((key) => (
-          <Selection key={key} applyColor={applyColor} />
+          <Selection
+            key={key}
+            applyColor={applyColor}
+          />
         ))}
       </div>
     </div>
--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -1,11 +1,9 @@
-import React from "react";
-
 const ColourSelector = (props) => {
   const { config, selectNextBackground } = props;
   const { background } = config;
   return (
     <button
-      className={config.className}
+      className={config.classname}
       onClick={() => selectNextBackground({ background: background })}
     >
       {config.label}
